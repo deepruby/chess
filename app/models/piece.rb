12ba94953a5036e3c49a9c269c6740e81d8bb89e
James Foster <jamesfoster85@gmail.com>
2015-11-21 12:04:55 -0500

    DRYer flat_position method, added comments

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,12 +3,17 @@ class Piece < ActiveRecord::Base
   belongs_to :game
 
   def flat_position
-  	self.x_position + self.y_position * 8
+  	#self.x_position + self.y_position * 8
+    flat_destination(self.x_position, self.y_position)
   end
 
   def flat_destination(x,y)
   	x + y * 8
   end
+  # Instead of calculating x + y * 8, Consider using a 2D array instead:
+  # Coords = [[0,1,2,3,4,5,6,7],[8,9,10,11...]...[...60,61,62,63]]
+  # Then call Coords[y][x]
+  # Coords[0][0] => 0, Coords[3][5] => 29, Coords[7][7] => 63
 
   def distance_to(x,y)
   	[(self.x_position - x).abs, (self.y_position - y).abs].max
