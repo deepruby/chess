0aa426d3ea7bc8dfd4b83f9a3a50ec17831f08bf
Ken Mak <kenwmak@gmail.com>
2015-12-05 21:59:11 +0000

    update logic and added test

--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -30,18 +30,59 @@ class PieceTest < ActiveSupport::TestCase
     assert_not @white_rook.is_obstructed?(2,7)
   end
 
+  test "capture - valid, opponent piece" do
+    # white_bishop is at [0,5] from initialize_board
+    @black_bishop.capture_opponent!(0,5)
+    @white_bishop.reload
+    assert_equal [nil,nil], [@white_bishop.x_position, @white_bishop.y_position]
+  end
+
+  test "capture - invalid, own piece" do
+    # black_rook is at [0,0] from initialize_board
+    @black_bishop.capture_opponent!(0,0)
+    @black_rook.reload
+    assert_equal [0,0], [@black_rook.x_position, @black_rook.y_position]
+  end
+
+  test "move - invalid" do
+    @black_king.move!(4,2)
+    @black_king.reload
+    assert_equal [4,0], [@black_king.x_position, @black_king.y_position]
+  end
+
+  test "move - valid" do
+    @black_king.update_attributes(:x_position => 4, :y_position => 2)
+    @black_king.move!(4,3)
+    @black_king.reload
+    assert_equal [4,3], [@black_king.x_position, @black_king.y_position]
+  end
+
+  test "move - valid with capture" do
+    @black_king.update_attributes(:x_position => 1, :y_position => 5)
+    @black_king.move!(1,6)
+    @black_king.reload
+    @white_pawn2.reload
+    assert_equal [1,6], [@black_king.x_position, @black_king.y_position]
+    assert_equal [nil,nil], [@white_pawn2.x_position, @white_pawn2.y_position]
+  end
+
   private
 
   def initialize_board
+    @black_player = FactoryGirl.create(:user) 
+    @white_player = FactoryGirl.create(:user) 
+
     # create a game and the 32 pieces, select pieces to test
-    @game = Game.create(:name => 'lolomg')
+    @game = Game.create(:name => 'lolomg', :black_player => @black_player, :white_player => @white_player)
     @black_bishop = @game.pieces.find{|piece| piece.x_position == 5 && piece.y_position == 0}
     @black_pawn = @game.pieces.find{|piece| piece.x_position == 2 && piece.y_position == 1}
     @black_rook = @game.pieces.find{|piece| piece.x_position == 0 && piece.y_position == 0}
+    @black_king = @game.pieces.find{|piece| piece.x_position == 4 && piece.y_position == 0}
     @white_bishop = @game.pieces.find{|piece| piece.x_position == 2 && piece.y_position == 7}
     @white_knight = @game.pieces.find{|piece| piece.x_position == 1 && piece.y_position == 7}
     @white_rook = @game.pieces.find{|piece| piece.x_position == 0 && piece.y_position == 7}
     @white_pawn = @game.pieces.find{|piece| piece.x_position == 0 && piece.y_position == 6}
+    @white_pawn2 = @game.pieces.find{|piece| piece.x_position == 1 && piece.y_position == 6}
     # remove the pawn at A7 from the board
     @white_pawn.destroy
     # move the bishop from C8 to A6 
