8b20f0bf6e89cea59b60d0aa497af57b8ec6acb9
James Foster <jamesfoster85@gmail.com>
2015-12-08 10:44:24 -0500

    Updated tests, original king tests were invalid moves

--- test/models/king_test.rb
+++ test/models/king_test.rb
@@ -4,21 +4,21 @@ class KingTest < ActiveSupport::TestCase
 	setup :game_and_king
 
   test 'Legal vertical move' do
-    assert @king.legal_move?(4, 1)
+    assert @king.legal_move?(4, 3)
   end
 
   test 'Legal diagonal move' do
-    assert @king.legal_move?(5, 1)
+    assert @king.legal_move?(5, 3)
   end
 
   test 'Illegal move' do
-    assert_not @king.legal_move?(5, 2)
+    assert_not @king.legal_move?(5, 4)
   end  
 
 	private
 
   def game_and_king
     @game = Game.create(:name => 'lolomg', :white_player_id => 1)
-    @king = @game.pieces.find_by(type: 'King', player_id: 1)
+    @king = @game.pieces.create(type: 'King', x_position: 4, y_position: 2)
   end
 end
